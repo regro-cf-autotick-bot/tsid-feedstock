576afd902bd5fa65f3526ce91773238047de7b59
Justin Carpentier <justin.carpentier@inria.fr>
2022-11-19 10:06:41 +0100

    oxs: fix for oxs-64 and boost 1.78

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,12 @@
 mkdir build
 cd build
 
+# Workaround for https://github.com/conda-forge/gazebo-feedstock/pull/150
+# Remove when boost is updated to 1.80.0
+if [[ "${target_platform}" == "osx-64" ]]; then
+  export CXXFLAGS="-DBOOST_ASIO_DISABLE_STD_ALIGNED_ALLOC ${CXXFLAGS}"
+fi
+
 cmake .. \
       ${CMAKE_ARGS} \
       -DCMAKE_BUILD_TYPE=Release \
